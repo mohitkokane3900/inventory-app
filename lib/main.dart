6d147cecaebaad29737940d3d783c79d783003aa
Mohit Kokane <mohitkokane12@gmail.com>
2025-11-06 23:51:54 -0500

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,13 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('✅ Firebase Connected'))),
-    );
-  }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(child: Text('Firebase Connected')),
+    ),
+  ));
 }
